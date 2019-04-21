9b652f7f8ceca428be266348fdf403e2acd9671a
Felipe Nipo <felipe.nipo@hbc.com>
2019-04-21 11:54:04 +0100

    Writer monad

--- src/main/scala/monads/Program.scala
+++ src/main/scala/monads/Program.scala
@@ -1,9 +1,26 @@
 package monads
 
-import cats.Eval
+import cats._
+import cats.implicits._
 import monads.StackSafety.foldRight
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Program extends App {
   val foldResult = foldRight((1 to 100000).toList, 0L)(_ + _)
   println(foldResult.value)
+
+  val r = Await.result(Future.sequence(Vector(
+    Future(Writer.factorial(3)),
+    Future(Writer.factorial(3))
+  )), 5.seconds)
+  println(r)
+
+  val r2 = for {
+    a <- Writer.wFactorial(3)
+    b <- Writer.wFactorial(3)
+  } yield a + b
+  r2.written.foreach(println(_))
 }
